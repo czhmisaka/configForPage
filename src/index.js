531607e973cd2f601a5905afa068ae63c694ebe9
陈之罕 <wb-czh931449@cainiao.com>
2021-11-16 14:14:12 +0800

    czh
    Signed-off-by: 陈之罕 <wb-czh931449@cainiao.com>

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 /*
  * @Author: czh
  * @Date: 2021-08-31 20:28:07
-<<<<<<< HEAD
  */
 
-=======
- */
->>>>>>> origin/master
+
+// 输入插件
+const ConfigForPage = () => {
+    this.install = function () {
+
+    }
+}
+
+
+export default ConfigForPage
--- src/util/importApi.js
+++ src/util/importApi.js
@@ -5,16 +5,51 @@
  */
 
 
+const fs = require('fs');
+
+/**
+ * @Author: czh
+ * @name: _getPathInfo
+ * @Description: Do not show any view
+ * @Date: 2021-11-11 16:23:37
+ * @param {src:String,needDeep:Boolean,endTag:String}
+ * @type {needDeep为是否进行深度搜索,endTag为文件后缀名}
+ * @return {*}
+ */
+const _getPathInfo = (src, needDeep = false, endTag = '.js', pathList = []) => {
+    let needSearchList = []
+    fs.readdirSync(src, {
+        withFileTypes: true
+    }).map(x => {
+        if (x.isDirectory()) needSearchList.push(src + x.name + '/')
+        else pathList.push(src + x.name)
+    })
+    if (!needDeep) return pathList;
+    needSearchList.map(x => [
+        pathList = _getPathInfo(x, needDeep, endTag, pathList)
+    ])
+    return pathList;
+}
+
 /**
  * @Author: czh
  * @name: searchApiFromJS
  * @Description: 从指定路径中搜索Api进行注入
  * @Date: 2021-10-14 11:15:11
- * @param {*}
+ * @param {pathList<Object>}
+ * @type {{pathlist参数示例:[{[.src,needDeep,endTag]}]}}
  * @return {*}
+ * 等待到适配vue3的时候进行ts化
  */
-const searchApiFromJS = (path = './') => {
-
+const searchApiFromJS = (pathList = []) => {
+    if (!pathList || pathList.length == 0) return console.error('Api加载错误：【searchApiFromJS】', pathList);
+    modules = [];
+    pathList.map(x => {
+        _getPathInfo(x.src, x.needDeep || false, x.endTag || '.js').map((y) => {
+            modules.push(require(y))
+        })
+    })
+    return modules;
 }
 
 
@@ -28,16 +63,19 @@ const searchApiFromJS = (path = './') => {
  * @return {*}
  */
 const searchModulesByPath = (path = null) => {
-    
+
 }
 
 
 
 
+const test = [{
+    src: './',
+    needDeep: false
+}]
+console.log(searchApiFromJS(test))
 
-
-
-
-export default {
-
+module.exports = {
+    searchApiFromJS,
+    searchModulesByPath
 }
